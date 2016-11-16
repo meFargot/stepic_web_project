0f60de4304f99e6305fea58be91caae9bcb2b6f7
meFargot <me.fargot@ya.ru>
2016-11-16 20:13:58 +0000

    Finnaly

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo apt install mc
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 git config --global user.email "me.fargot@ya.ru"
 git config --global user.name "meFargot"
