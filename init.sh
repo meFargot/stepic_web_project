88f8a0447974fb8247e6d3a49b96ed127329e89a
meFargot <me.fargot@ya.ru>
2016-11-16 19:26:10 +0000

    New init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo apt install mc
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+git config --global user.email "me.fargot@ya.ru"
+git config --global user.name "meFargot"
