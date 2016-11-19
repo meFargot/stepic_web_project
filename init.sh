87c09c55723a2384726ca415bad259f6e1da9bfb
meFargot <me.fargot@ya.ru>
2016-11-19 08:13:35 +0000

    Success

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo apt install mc
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 git config --global user.email "me.fargot@ya.ru"
